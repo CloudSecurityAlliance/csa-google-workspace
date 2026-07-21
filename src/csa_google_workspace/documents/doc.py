1f1318efe12f418af57893bfd7ef733b5d254272
Kurt Seifried <kurt@seifried.org>
2026-07-21 15:27:24 -0600

    feat: Doc.as_text + Doc.paragraphs (Docs content read)

--- src/csa_google_workspace/documents/doc.py
+++ src/csa_google_workspace/documents/doc.py
@@ -1,5 +1,13 @@
 from ..base import Document
+from .. import _content
 
 
 class Doc(Document):
-    """Google Docs. Content read/write + suggestions arrive in later phases."""
+    """Google Docs. Content read; write + suggestions arrive in later phases."""
+
+    def as_text(self) -> str:
+        return _content.doc_text(self._backend.get_document(self.id))
+
+    @property
+    def paragraphs(self) -> list[str]:
+        return _content.doc_paragraphs(self._backend.get_document(self.id))
